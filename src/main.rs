e50b8185a48e4f929a0158814f08b8b150fbd473
b-x-wu <bridgette.x.wu@gmail.com>
2025-06-26 16:21:29 -0700

    fix: remove container config

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,5 @@ fn ping() -> &'static str {
 fn rocket() -> _ {
     rocket::build()
         .mount("/api", routes![ping])
-        .mount("/", rocket::fs::FileServer::from("/app/static"))
+        .mount("/", rocket::fs::FileServer::from("/static"))
 }
